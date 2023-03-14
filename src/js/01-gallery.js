e8e420dc2e87d88f4b3b586ed4b016ebd99fd609
EvgeniiStepanenko <115147549+EvgeniiStepanenko@users.noreply.github.com>
2023-03-14 12:19:36 +0200

    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+const divEl = document.querySelector('.gallery');
+
+
+
+
+function createGalleryMurkup (items ){
+    return items
+    .map((item) =>`
+    
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" 
+            alt="${item.description}" />
+        </a>
+    `
+  )
+  .join("");
+};
+
+
+const addGalleryMurkup = createGalleryMurkup(galleryItems);
+
+divEl.innerHTML = addGalleryMurkup;
+
+
+new SimpleLightbox(".gallery a", {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    scrollZoom: false,
+   });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import  Player  from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+
+
+const player = new Player('vimeo-player');
+
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000)
+);
+
+
+
+
+const playerCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+if (playerCurrentTime) {
+  player
+  .setCurrentTime(playerCurrentTime)
+  .then(function () {
+    console.log('Video start time set to ' + playerCurrentTime + ' seconds');
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+
+function playFromLocalStorage() {
+  player
+  .play()
+  .then(function () {
+    console.log('Video resumed from ' + playerCurrentTime + ' seconds');
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+
+// Listen for click on the play button
+const playButton = document.getElementById('play-button');
+playButton.addEventListener('click', playFromLocalStorage);
